d7600eee99bb3f02979a552bedcf076435d2a40a
Shubhr457 <136572711+Shubhr457@users.noreply.github.com>
2024-08-06 22:55:23 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,8 +13,8 @@ module.exports = {
   },
   networks: {
     amoy: {
-      url: "https://polygon-amoy.g.alchemy.com/v2/hXwE8n7cqsBK_9BwpPmlQ4nmTLh3t3Jd", // Replace with the actual Amoy testnet URL
-      accounts: ["eedd5eed4e6aab6ae6987b0dd6e700890907e2ee21c080037430bfe44cdf82d7"] // Replace with your testnet account private key
+      url: "https://polygon-amoy.g.alchemy.com/v2/hXwE8n7cqsBK_9BwpPmlQ4nmTLh3t3Jd", 
+      accounts: ["eedd5eed4e6aab6ae6987b0dd6e700890907e2ee21c080037430bfe44cdf82d7"] 
     }
   }
 };
