e29b552c352a9f7bd966bc85385f7e5cd97b1212
Shubhr457 <136572711+Shubhr457@users.noreply.github.com>
2024-08-07 15:48:39 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
+//require('@nomiclabs/hardhat-etherscan');
+//require("@nomiclabs/hardhat-waffle");
+//require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,8 +16,22 @@ module.exports = {
   },
   networks: {
     amoy: {
-      url: "https://polygon-amoy.g.alchemy.com/v2/hXwE8n7cqsBK_9BwpPmlQ4nmTLh3t3Jd", 
-      accounts: ["eedd5eed4e6aab6ae6987b0dd6e700890907e2ee21c080037430bfe44cdf82d7"] 
+      url: "https://polygon-amoy.g.alchemy.com/v2/hXwE8n7cqsBK_9BwpPmlQ4nmTLh3t3Jd", // Replace with the actual Amoy testnet URL
+      accounts: ["eedd5eed4e6aab6ae6987b0dd6e700890907e2ee21c080037430bfe44cdf82d7"] // Replace with your testnet account private key
     }
+  },
+  etherscan: {
+    apiKey: "BDVG74Z27JY6P2BXAI6FNE3KFRI3GF76ZY",
+    customChains: [
+      {
+        network: "amoy",
+        chainId: 80002, // Replace with your chain ID
+        urls: {
+          apiURL: "https://api-testnet.polygonscan.com/api",
+          browserURL: "https://mumbai.polygonscan.com"
+        }
+      }
+    ]
   }
+  
 };
