707d4c359b64b38a879ad9fca58c4f896376d23b
Shubhr457 <136572711+Shubhr457@users.noreply.github.com>
2024-08-07 10:11:51 +0530

    Update deploy_nft_marketplace.js

--- ignition/modules/deploy_nft_marketplace.js
+++ ignition/modules/deploy_nft_marketplace.js
@@ -2,6 +2,12 @@ async function main() {
     const NFTStore = await ethers.getContractFactory("NFTSTORE");
     const nftStore = await NFTStore.deploy();
     console.log("NFTStore deployed to:", nftStore.address);
+
+     await run("verify:verify", {
+    address: NFTSTORE.address,
+    constructorArguments: [],
+  });
+}
   }
   
   main()
